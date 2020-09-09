52f73cd896386a36fe41830a4f39e636c2cbc476
Simranjeet Singh Sahni <simranjeetsinghsahni@Simranjeets-MacBook-Pro.local>
2020-09-09 01:38:19 -0400

    uploading final files for the webApp

---
+++ src/Firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const firebaseConfig = {};
+
+// Initialize Firebase with a "default" Firebase project
+var firebaseProject = firebase.initializeApp(firebaseConfig);
+
+console.log(firebaseProject.name); // "[DEFAULT]"
+
+// Option 1: Access Firebase services via the defaultProject variable
+// var defaultStorage = defaultProject.storage();
+var db = firebaseProject.firestore();
+
+// Option 2: Access Firebase services using shorthand notation
+// defaultStorage = firebase.storage();
+// constdefaultFirestore = firebase.firestore();
+
+export { db };
---
+++ src/Row.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import requests from "./request";
+import "./Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const base_url = "https://image.tmdb.org/t/p/w500/";
+
+function Row({ title, fetchURL, isLarge }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchURL]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  console.table(movies);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row__poster ${isLarge && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLarge ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+}
+
+export default Row;
